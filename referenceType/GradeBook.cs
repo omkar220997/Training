9c71658904663d3905c30f08f13cbd6fce005f27
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 10:55:45 +0530

    Gradebook code is completed

---
+++ referenceType/GradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace referenceType
+{
+    public class GradeBook
+    {
+        public GradeBook(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        public void GetStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var numbers in grades)
+            {
+                highGrade = Math.Max(numbers, highGrade);
+                lowGrade = Math.Min(numbers, lowGrade);
+                result += numbers;
+            }
+            result /= grades.Count;
+            Console.WriteLine("The average grade is " + result);
+            Console.WriteLine("The highest grade is " + highGrade);
+            Console.WriteLine("The lowest grade is " + lowGrade);
+
+        }
+        private List<double> grades;
+        public string Name;
+
+
+
+    }
+}
---
+++ referenceType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace referenceType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new GradeBook("Omkar's Grade book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.9);
+            book.AddGrade(77.1);
+            book.GetStatistics();
+
+            Console.ReadLine();
+
+            
+
+           
+        }
+    }
+}

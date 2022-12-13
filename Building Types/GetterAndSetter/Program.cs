74018b97c132de26707b9772380a441cc24a10aa
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 16:25:20 +0530

    Getter and Setter method program

---
+++ Building Types/GetterAndSetter/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterAndSetter
+{
+    public class Employee
+    {
+        private string name;
+        private int age ;
+
+        public string Name
+        {
+            get 
+            { 
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) 
+                {
+                    name = value;
+                }
+                
+            }
+
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if(value > 0) 
+                { 
+                    age = value;
+                }
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Prasad";
+            employee.Age = 25;
+            Console.WriteLine($"Name of the employee is {employee.Name}");
+            Console.WriteLine($"Age of the employee is {employee.Age}");
+            Console.ReadLine();
+        }
+    }
+}

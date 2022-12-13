d718c98ef0aef76909553ba355bffa811604a120
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 12:00:46 +0530

    referencr type with parameters

---
+++ RefType/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // object created using new keyword
+            child first = new child(5, "Akash");
+            child second = new child(7, "Shubham");
+
+            //object created using default constructor 
+            child third = new child();
+
+            // output
+            Console.Write("Child First: ");
+            first.ChildInfo();
+            Console.Write("Child Second: ");
+            second.ChildInfo();
+            Console.Write("Child Third: ");
+            third.ChildInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RefType/child.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefType
+{
+    public class child
+    {
+        private int age;
+        private string name;
+
+        public child()
+        {
+            name = "N/A";
+        }
+
+        public child (int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public void ChildInfo()
+        {
+            Console.WriteLine($"{name} is {age} years old...");
+        }
+
+    }
+}

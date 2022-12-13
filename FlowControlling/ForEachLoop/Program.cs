64df380de248313871e576e98066c84dfb4fd533
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 14:57:18 +0530

    ForEach loop program

---
+++ FlowControlling/ForEachLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachLoop
+{
+    public class Marks
+    {
+        public static int maximum(int[] marks)
+        {
+            int maxx = marks[0];
+            foreach(int num in marks)
+            {
+                if(num > maxx)
+                {
+                    maxx = num;
+                }
+            }
+            return maxx;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = { 20, 22, 25, 24, 27, 23 };
+            int maxmum= Marks.maximum(marks);
+            Console.WriteLine($"Highest marks is {maxmum}");
+            Console.ReadLine();
+            
+
+        }
+    }
+}

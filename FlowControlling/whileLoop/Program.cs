92634e53946e5c4debd4f36fc2eb6b16042d3456
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 14:16:06 +0530

    While loop

---
+++ FlowControlling/whileLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoop
+{
+    public class Authority
+    {
+        private int x;
+
+        public Authority(int x)
+        {
+            this.x = x;
+        } 
+
+        public void Value()
+        {
+            while (x < 18)
+            {
+                Console.WriteLine($"Not an adult at {x} age");
+                x++;
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Authority age = new Authority(10);
+            age.Value();
+            Console.ReadLine();
+        }
+    }
+}

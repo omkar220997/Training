106eb1ac8d37a639723d0aecf5c144513ade8432
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-13 14:21:16 +0530

    Do-While loop Program

---
+++ FlowControlling/doWhileLoop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileLoop
+{
+    public class Age
+    {
+        private int x;
+
+        public Age(int x)
+        {
+            this.x = x;
+        }   
+
+        public void GetAge()
+        {
+            do
+            {
+                Console.WriteLine($"you are only {x} years old After the age of 18 years you are an adult");
+                x++;
+            }
+            while (x<18);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Age child = new Age(15);
+
+            child.GetAge();
+            Console.ReadLine();
+        }
+    }
+}

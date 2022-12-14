b99217217bbf4d259645368bdc70f77edfbe3bab
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-14 15:03:47 +0530

    Method with no parameter with return type

---
+++ 14-12-2022/Methods/Method3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    public class Program
+    {
+        public int square()
+        {
+            int side = 20;
+            int area = side * side;
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area of square is :");
+            Program p =new Program();
+            Console.WriteLine(p.square());
+            Console.ReadLine(); 
+        }
+    }
+}

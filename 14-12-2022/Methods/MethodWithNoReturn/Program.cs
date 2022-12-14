f53b35b3007d4c9af257ed3edf66835c00702f9e
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-14 15:00:11 +0530

    Method with no return type

---
+++ 14-12-2022/Methods/MethodWithNoReturn/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithNoReturn
+{
+    
+    public class Program
+    {
+        public void Area(int l, int b)
+        {
+            {
+                int a = l * b;
+                Console.WriteLine("Area of rectangle is " + a);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter lenght of rectangle");
+            string lstr=Console.ReadLine();
+            int l=Convert.ToInt32(lstr);
+            Console.WriteLine("Enter width of rectangle");
+            string bstr=Console.ReadLine();
+            int b=Convert.ToInt32(bstr);
+
+            Program p=new Program();
+            p.Area(l, b);
+            Console.ReadLine();
+        }
+    }
+}

f2086a7f6f6b00ee9dd5c295f99dcb488ae7aef5
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-14 15:07:22 +0530

    method with parameter and no return type

---
+++ 14-12-2022/Methods/Method4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    public  class Program
+    {
+        public void car(string name)
+        {
+            Console.WriteLine("Name of car is " +name);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.car("Swift");
+            Console.ReadLine(); 
+        }
+    }
+}

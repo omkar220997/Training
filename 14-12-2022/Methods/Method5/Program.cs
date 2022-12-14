5d11bf7262a8f8d9857ce4b54874509469737bbd
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-14 15:11:49 +0530

    Static method with return type and parameter

---
+++ 14-12-2022/Methods/Method5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method5
+{
+    public class Program
+    {
+        static string Student (string name)
+        {
+            Console.Write ("Name of student is : ");
+            return name;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name of student:");
+            string name=Console.ReadLine();
+            Console.WriteLine(Student (name));
+            Console.ReadLine();
+        }
+    }
+}

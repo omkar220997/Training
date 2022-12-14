58a63d4f5a1c3715b34d76a6eb7e3787135774fa
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-14 11:50:41 +0530

    second program of abstraction

---
+++ 14-12-2022/object oriented programming/Abstraction2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction2
+{
+    
+    public abstract class Shape
+    {
+        public abstract void Area();
+    }
+    
+    public class Circle : Shape
+    {
+        public override void Area()
+        {
+            double a = 3.14 * rad * rad;
+            Console.WriteLine("Area of circle is " +a);
+            
+        }
+
+        public int rad;
+      
+         public int Rad { get; set; }
+        
+
+        public Circle( int rad)
+        {
+            this.rad = rad;
+            
+            Console.WriteLine("Radius of circle is " + rad);
+            
+            
+            
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(10);
+            c.Area();
+            Console.ReadLine();
+
+        }
+    }
+}

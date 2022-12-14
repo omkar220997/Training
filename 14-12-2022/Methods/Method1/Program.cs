524f55a82264c64a29de016312b57b5afe73441f
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-14 14:55:10 +0530

    Method basic program

---
+++ 14-12-2022/Methods/Method1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    public  class Program
+    {
+        public int sum (int x,int y)
+        {
+            Console.Write("Sum of x and y is : " );
+            return (x + y);
+        } 
+        public int sub(int x,int y)
+        {
+            Console.Write("Sub of x and y is : " );
+
+            return (x - y);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value of x:");
+            string xstr=Console.ReadLine();
+            int x=int.Parse(xstr);
+            Console.WriteLine("Enter a value of y:");
+            string ystr=Console.ReadLine();
+            int y =int.Parse(ystr);
+            Program p = new Program();
+            Console.WriteLine(p.sum(x,y));
+            Console.WriteLine(p.sub(x,y));
+            Console.WriteLine("Addition and Substraction");
+            Console.ReadLine();
+
+        }
+    }
+}

435eda24834c14d67a5ae3d8212d97cf1db293b4
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-15 17:11:04 +0530

    creating a string and making it user input

---
+++ 15-12-2022/Strings/StringsPractice/Information.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsPractice
+{
+    internal class Information
+    {
+        string firstName;
+        string lastName;
+        string email;
+        string MobileNumber;
+        public void FormDetails()
+        {
+            Console.WriteLine("Enter a first name");
+             firstName = Console.ReadLine();
+            Console.WriteLine("Enter a last name");
+             lastName = Console.ReadLine();
+            Console.WriteLine("Enter a Email");
+             email = Console.ReadLine();
+            Console.WriteLine("Enter a Mobile Number");
+             MobileNumber= Console.ReadLine();
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("First name is :" + firstName);
+            Console.WriteLine("Last name is :" + lastName);
+            Console.WriteLine("Email is " + email);
+            Console.WriteLine("Mobile Number is " + MobileNumber);
+
+        }
+    }
+}

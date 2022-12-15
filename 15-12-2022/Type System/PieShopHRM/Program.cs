dd557445780cc61d4df431b7036d82ecf154ddfd
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-15 16:48:50 +0530

    Practicing various commands in this project

--- 15-12-2022/Type System/PieShopHRM/Program.cs
+++ 15-12-2022/Type System/PieShopHRM/Program.cs
@@ -20,6 +20,18 @@ namespace PieShopHRM
             double numberOfHoursWorked = 220;
             
             double currentMonthWage=ratePerHour*numberOfHoursWorked;
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            DateTime Joindate = new DateTime(2022, 12,05 ,09, 00, 0);
+            Console.WriteLine(Joindate);
+
+            double d = 245369.252;
+            int x = Convert.ToInt32(d);
+            Console.WriteLine("Value of x is " +x);
+
+            int a = 10;
+            double b = a;
+            Console.WriteLine("value of b is " + b);
             if(numberOfHoursWorked > 200)
             {
                 currentMonthWage+=bonus;

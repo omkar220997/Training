7d8c2375e9027973ab586913e42327a1eba93198
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-16 13:31:54 +0530

    Length of string program

---
+++ 16-12-2022/string programs/StringLength/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your String here");
+            string InputString=Console.ReadLine();
+            int LengthOfString=InputString.Length;
+            Console.WriteLine("Length of string is " +LengthOfString);
+            Console.ReadLine();
+        }
+    }
+}

b433b5c09c62f9bb50df741aea25eb6ef434245c
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-16 14:13:09 +0530

    Issue have in the program

---
+++ 16-12-2022/string programs/FrequencyOfSpecificWord/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyOfSpecificWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your String Here");
+            string InputString = Console.ReadLine();
+            Console.WriteLine("Enter a word to find Frequency");
+            string InputCharacter = Console.ReadLine();
+            string OutputString = string.Empty;
+            for (int i = 0; i < InputString.Length; i++)
+            {
+                if (InputString.Contains(InputCharacter))
+                {
+                    OutputString+=InputString[i];
+                }
+            }
+            Console.WriteLine(OutputString);
+            int FrequencyCount = OutputString.Length;
+            
+            Console.WriteLine($"Frquency of {InputCharacter} is {FrequencyCount}");
+            Console.ReadLine();
+        }
+    }
+}

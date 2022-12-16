00959652c2e552e2f14f268bee902103c40fa764
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-16 18:19:06 +0530

    Constructor basic

---
+++ 16-12-2022/Constructors/ConstructorsBasic/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsBasic
+{
+    internal class Employee
+    {
+        string EmployeeName;
+        int EmployeeId;
+
+        protected internal Employee()
+        {
+            Console.WriteLine("Enter a name of Employee");
+            EmployeeName =Console.ReadLine();
+            Console.WriteLine("Enter employee id of Employee");
+            EmployeeId = Convert.ToInt32(Console.ReadLine());
+        }
+        protected internal void Details()
+        {
+            
+            Console.WriteLine("Name of the employee is " + EmployeeName);
+            Console.WriteLine("Employee id of employee is " +EmployeeId);
+        }
+    }
+}
---
+++ 16-12-2022/Constructors/ConstructorsBasic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsBasic
+{
+    internal class Program
+    {
+        int i; bool b;
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            //the value of i and b is defined by implicit constructor
+            // which is implicitly created by the compiler.
+            Console.WriteLine("Value of i is " +p.i);
+            Console.WriteLine("Value of b is " +p.b);
+            // the value of EmployeeName and EmployeeId is assigned by programmer 
+            // in explicitly called constructor and the Details method is used to display the values.
+            Employee employee = new Employee();
+            employee.Details();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

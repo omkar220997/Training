92d3d38f720064caf272f075f2d48dc777c10cf0
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-16 12:25:17 +0530

    Lenght of Longest Substring without repeating characters LeetCode probelm

---
+++ 16-12-2022/string programs/LongestSubStringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubStringWithoutRepeatingCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string here ");
+            string InputString=Console.ReadLine();
+            string OutputString=string.Empty;
+            int LenghtOfLongestSubstring;
+            for(int i = 0; i < InputString.Length; i++)
+            {
+                if (!OutputString.Contains(InputString[i]))
+                {
+                    OutputString += InputString[i];
+                }
+
+            }
+
+            OutputString = OutputString.Replace(" ", "");
+            Console.WriteLine(OutputString);
+            LenghtOfLongestSubstring=OutputString.Length;
+            Console.WriteLine(LenghtOfLongestSubstring);
+            Console.ReadLine();
+        }
+    }
+}

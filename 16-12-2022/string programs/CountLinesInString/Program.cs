7347dc816b3ab07221f3cbf320d06905c1a4adf1
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-16 13:39:34 +0530

    Count the lines in a string

---
+++ 16-12-2022/string programs/CountLinesInString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLinesInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int start = 0;
+            string InputString = "Hello \n Omkar kadam \n Good Morning.";
+            while((start=InputString.IndexOf('\n', start)) != -1)
+            {
+                count++;
+                start++;
+            }
+            Console.WriteLine("Numbers of line is " +count);
+            Console.ReadLine();
+        }
+    }
+}

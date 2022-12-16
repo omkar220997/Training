31b3a8d3fd99d8a780564a8b05b2c5ad9da2da3c
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-16 09:28:14 +0530

    Escape Characters

---
+++ 15-12-2022/Strings/Escapecharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escapecharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Omkar";
+            string lastName = "Kadam";
+            // showing file path
+            string FilePath = "C:\\desktop\\Training\\String.sln";
+            Console.WriteLine("first name is " +firstName);
+            Console.WriteLine("last name is " +lastName);
+            Console.WriteLine($"{firstName} {lastName}");
+            Console.WriteLine($"Welcom\n{firstName} \t{lastName}\nGood Morning");
+            Console.WriteLine("Path of the file is " +FilePath);
+           
+            Console.ReadLine();
+        }
+    }
+}

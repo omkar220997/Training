4e4a3856d73bbdfe68d198595f14cef0fb9c9733
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-19 16:40:30 +0530

    Add method of list in generic collection

---
+++ 19-12-2022/Collections/ListInGenericCollections/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListInGenericCollections
+{
+    internal class Customer
+    {
+        private string CustomerName;
+        private double CustomerBill;
+
+        internal void AddCustomer()
+        {
+            Console.WriteLine("Enter the name of customer");
+            CustomerName = Console.ReadLine();
+            Console.WriteLine("Enter the bill amount of Customer");
+            CustomerBill =Convert.ToDouble( Console.ReadLine());
+        }
+        internal void ShowCustomer()
+        {
+            Console.WriteLine("Name of the customer is " + CustomerName);
+            Console.WriteLine("Bill of the customer is " + CustomerBill);
+        }
+    }
+}
---
+++ 19-12-2022/Collections/ListInGenericCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListInGenericCollections
+{
+    internal class Program
+    {
+        List<Customer> Customers = new List<Customer>();
+        
+        private void AddList()
+        {
+            var customer = new Customer();
+            customer.AddCustomer();
+            Customers.Add(customer);
+        }
+        private void CustomerDetails()
+        {
+            foreach (var customer in Customers)
+            {
+                customer.ShowCustomer();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.AddList();
+            p.CustomerDetails();
+            Console.ReadLine();
+            
+            
+
+            
+        }
+    }
+}

fca9bf1d39a0a56c22d724cd42a2870d4a6b6855
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-19 08:58:33 +0530

    Default Constructor

---
+++ 19-12-2022/Constructors/TypesOfConstructors/DefaultConstructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesOfConstructors
+{
+    internal class DefaultConstructor
+    {
+        int x = 10;
+        string name = "Omkar";
+
+        internal DefaultConstructor()
+        {
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My roll number is " + x);
+        }
+    }
+}
---
+++ 19-12-2022/Constructors/TypesOfConstructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesOfConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DefaultConstructor defaultConstructor = new DefaultConstructor();
+            Console.ReadLine();
+        }
+    }
+}

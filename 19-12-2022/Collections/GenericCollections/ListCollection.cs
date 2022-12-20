133c132fb812c85916c5a12ac1c914345d863c8a
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-20 10:54:34 +0530

    List Collection in Generics

---
+++ 19-12-2022/Collections/GenericCollections/ListCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class ListCollection
+    {
+        List<int> Marks=new List<int>();
+        internal void AddMarks()
+        {
+            Console.WriteLine("Enter marks of student1 ");
+            Marks.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter marks of student2 ");
+            Marks.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter marks of student3 ");
+            Marks.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter marks of student4 ");
+            Marks.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter marks of student5 ");
+            Marks.Add(Convert.ToInt32(Console.ReadLine()));
+        }
+        internal void DisplyMarks()
+        {
+            for(int i = 0; i < Marks.Count; i++)
+            {
+                Console.WriteLine($"Marks for student {i+1} is {Marks[i]}");
+            }
+        }
+
+
+    }
+}
---
+++ 19-12-2022/Collections/GenericCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListCollection listCollection = new ListCollection();
+            listCollection.AddMarks();
+            listCollection.DisplyMarks();
+            Console.ReadLine();
+        }
+    }
+}

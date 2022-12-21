b1caea98e30632e21cfe67eabe74f0f7e8b6cc14
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-21 15:42:35 +0530

    Delegate Practice problem

---
+++ 21-12-2022/Delegates/DelegateBasics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBasics
+{
+    internal class Program
+    {
+        public delegate void AddNumbersDelegate(int x, int y);
+        public delegate string NameDelegate(string namestr);
+        public void AddNumbers(int a, int b)
+        {
+            Console.WriteLine("Addition of A nd b is " +(a+b));
+        }
+        public static string Name(string name)
+        {
+            return name;
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            AddNumbersDelegate AD= new AddNumbersDelegate(p.AddNumbers);
+            AD(100, 50);
+            NameDelegate ND = new NameDelegate(Name);
+            string str = ND("Omkar Kadam");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}

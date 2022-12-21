284da9d7a97426915a4632ac10c20a732162b575
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-21 11:54:02 +0530

    Creating a Klingelnberg Customer managment Application with writing the business logic for customer class

---
+++ 21-12-2022/Object Oriented Programming/Application Based on OOPs/KlingelnbergCustomerManagment/KlingelnbergCustomerManagment.BusinessLogic/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlingelnbergCustomerManagment.BusinessLogic
+{
+    public class Customer
+    {
+        //Adding FirstName property using Auto implementation.
+        public string FirstName { get; set; }
+        // Adding LastName property using full syntax of nget and set property.
+        private string _lastName;
+        public string LastName
+        {
+            get 
+            {
+                return _lastName; 
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        // Adding Email property using the pre-defined snippet
+        public string EmailAdress { get; set; }
+        public int CustomerID { get; private set; }
+        public string FullName
+        {
+            get
+            {
+                return FullName + " " + LastName;
+            }
+        }
+
+    }
+}

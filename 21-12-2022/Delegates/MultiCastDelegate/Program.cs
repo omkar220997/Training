84226a94a26f28672d3dc5babe904bb240a53b34
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-21 15:58:09 +0530

    Practice program of multicast delegates

---
+++ 21-12-2022/Delegates/MultiCastDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCastDelegate
+{
+    internal class Program
+    {
+        public delegate void RectDelegate(double Length, double Width);
+        public void GetRectArea(double Length, double Width)
+        {
+            Console.WriteLine("Area of the rectangle is " + (Length * Width));
+        }
+        public void GetRectPerimeter(double Length, double Width)
+        {
+            Console.WriteLine("Perimeter of rectangle is " +(2*(Length+ Width)));
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            RectDelegate RD = p.GetRectArea;
+            RD += p.GetRectPerimeter;
+            RD(10, 15);
+            Console.ReadLine();
+        }
+    }
+}

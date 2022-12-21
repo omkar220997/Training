e0d62d05597b807ba95fbbca75c73f425229c31c
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-21 14:08:42 +0530

    Order class is completed in KCM>BL project of KCM application

---
+++ 21-12-2022/Object Oriented Programming/Application Based on OOPs/KlingelnbergCustomerManagment/KlingelnbergCustomerManagment.BusinessLogic/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlingelnbergCustomerManagment.BusinessLogic
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+        public Order(int orderID)
+        {
+            OrderID = orderID;
+        }
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderID { get; set; }
+
+        public Order Retrieve(int orderID)
+        {
+            return new Order();
+        }
+        public List<Order> Retrieve()
+        {
+            return new List<Order>();
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        public bool Validate()
+        {
+            var isValid = true;
+            if(OrderDate != null) isValid=false;
+            return isValid;
+
+        }
+    }
+}

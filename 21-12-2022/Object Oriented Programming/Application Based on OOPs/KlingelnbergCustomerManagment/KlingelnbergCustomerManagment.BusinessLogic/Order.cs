ffe3aa94f16ac6ffd71387e4257cd9fb1634b047
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-22 12:08:57 +0530

    modified Order class to establish composition between Customer class by using customerId and shipping address

--- 21-12-2022/Object Oriented Programming/Application Based on OOPs/KlingelnbergCustomerManagment/KlingelnbergCustomerManagment.BusinessLogic/Order.cs
+++ 21-12-2022/Object Oriented Programming/Application Based on OOPs/KlingelnbergCustomerManagment/KlingelnbergCustomerManagment.BusinessLogic/Order.cs
@@ -8,18 +8,23 @@ namespace KlingelnbergCustomerManagment.BusinessLogic
 {
     public class Order
     {
-        public Order()
+        public Order():this(0)
         {
 
         }
         public Order(int orderID)
         {
             OrderID = orderID;
+            OrderItems= new List<OrderItem>();
         }
+        public int CustomerID { get; set; }
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderID { get; private set; }
+        public List<OrderItem> OrderItems {get; set; }
+        public int ShippingAddressID { get; set; }
 
-        
+
+
         public bool Save()
         {
             return true;

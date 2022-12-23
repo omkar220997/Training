b89dd6d68cc485aead84d5535f9a75821038619c
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-23 16:16:05 +0530

    Integer to roman

---
+++ LeetCode/ContainerWithMostWater/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerWithMostWater
+{
+    internal class Program
+    {
+        public int MostWater(int[] height)
+        {
+            int Area=0;
+            int[] Cordinates1 = new int[2];
+            int[] Cordinates2 = new int[2];
+            List<int> CordinateOfX=new List<int>();
+            for(int i = 0; i < height.Length; i++)
+            {
+                Cordinates1[0] = i;
+                Cordinates1[1] = 0;
+                Cordinates2[0] = i;
+                Cordinates2[1] = height[i];
+                Console.WriteLine($"Cordinates for {i} is {Cordinates1[0]},{Cordinates1[1]} and {Cordinates2[0]},{Cordinates2[1]}");
+                double DistanceOfY = Math.Sqrt(Math.Pow(Cordinates2[0] - Cordinates1[0], 2) + Math.Pow(Cordinates2[1] - Cordinates1[1], 2));
+                Console.WriteLine("Distance is" + DistanceOfY);
+                CordinateOfX.Add(Cordinates1[0]);
+                CordinateOfX.Add(Cordinates1[1]);
+            }
+            foreach(int cordinate1 in CordinateOfX)
+            {
+                foreach(int cordinates in Cordinates1)
+                {
+                    Console.WriteLine(Cordinates1[0] + " " + Cordinates1[1]);
+
+                }
+            }
+            
+            
+            
+
+
+            return Area;
+        }
+        static void Main(string[] args)
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Program p =new Program();
+            p.MostWater(height);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCode/IntegerToRoman/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToRoman
+{
+    internal class Program
+    {
+        public string IntToRoman(int Number)
+        {
+            string RomanNumber=string.Empty;
+            while(Number >= 1000)
+            {
+                RomanNumber += "M";
+                Number= Number - 1000;
+               
+            }
+            while(Number<1000 && Number >= 900)
+            {
+                RomanNumber += "CM";
+                Number= Number - 900;
+               
+            }
+            while(Number<900 && Number >= 500)
+            {
+                RomanNumber += "D";
+                Number= Number - 500;
+               
+            }
+            while(Number<500 && Number >= 400)
+            {
+                RomanNumber += "CD";
+                Number= Number - 400;
+             
+            }
+            while(Number<400 && Number >= 100)
+            {
+                RomanNumber += "C";
+                Number= Number - 100;
+             
+            }
+            while(Number<100 && Number >= 90)
+            {
+                RomanNumber += "XC";
+                Number= Number - 90;
+                
+            }
+            while(Number<90 && Number >= 50)
+            {
+                RomanNumber += "L";
+                Number= Number - 50;
+                
+            }
+            while(Number<50 && Number >= 40)
+            {
+                RomanNumber += "XL";
+                Number= Number - 40;
+               
+            }
+            while(Number<40 && Number >= 10)
+            {
+                RomanNumber += "X";
+                Number= Number - 10;
+               
+            }
+            while(Number<10 && Number >= 9)
+            {
+                RomanNumber += "IX";
+                Number= Number - 9;
+               
+            } 
+            while(Number<9 && Number >= 5)
+            {
+                RomanNumber += "V";
+                Number= Number - 5;
+               
+            }
+            while(Number<5 && Number >= 4)
+            {
+                RomanNumber += "IV";
+                Number= Number - 4;
+               
+            }
+            while(Number<5 && Number >= 1)
+            {
+                RomanNumber += "I";
+                Number= Number - 1;
+               
+            }
+            return RomanNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Enter a Number here");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(p.IntToRoman(Number));
+            Console.ReadLine();
+        }
+    }
+}

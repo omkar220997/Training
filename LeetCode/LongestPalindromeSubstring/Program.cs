bae667d6cc4c1f5e33a56949a1a0a8eedd2046fa
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-23 10:22:50 +0530

    Pali9ndrome Sub strings from inpout string

---
+++ LeetCode/LongestPalindromeSubstring/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestPalindromeSubstring
+{
+    internal class Program
+    {
+        public string LongestPalindrome(string s)
+        {
+            string SubString=string.Empty;
+            string ReverseSubString="";
+            for (int i = 0; i < s.Length + 1; i++)
+            {
+                for (int j = 0; j < s.Length + 1 - i; j++)
+                {
+                    SubString = s.Substring(i, j);
+                    //Console.WriteLine($"Substring started from {i} to {j} is {SubString}");
+                    int len = SubString.Length - 1;
+                    ReverseSubString = string.Empty;
+                    int length = 0;
+                    while (len >= 0)
+                    {
+
+                        ReverseSubString += SubString[len];
+                        len--;
+
+                    }
+                    //Console.WriteLine("reverse of substring is " + ReverseSubString);
+                    if(SubString.Length > length)
+                    {
+                         length= SubString.Length;
+                        if(SubString!=""&& ReverseSubString.Equals(SubString))
+                        {
+                            Console.WriteLine("Longest Palindromew Substring is " +SubString);
+                        }
+                    }
+
+                }
+
+            }
+           
+            return SubString;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string s = "abcdcbef";
+            p.LongestPalindrome(s);
+
+            Console.ReadLine();
+        }
+    }
+}

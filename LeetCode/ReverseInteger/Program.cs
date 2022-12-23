55c717ef9e376c2869a48df82b6ec5430ca495bf
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-23 11:13:57 +0530

    Reverse integer taking as a string and takijng as a interger

---
+++ LeetCode/ReverseInteger/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger
+{
+    internal class Program
+    {
+        public int ReverseIntegerNumber(int Number)
+        {
+            string Numberstr=Convert.ToString(Number);
+            string ReverseNumberstr = string.Empty;
+            string Sign="";
+            if (Numberstr.Contains("-"))
+            {
+                 Sign = "-";
+                for (int i = Numberstr.Length - 1; i > 0; i--)
+                {
+
+                    ReverseNumberstr += Numberstr[i];
+                }
+
+            }
+            else
+            {
+                for (int i = Numberstr.Length - 1; i >= 0; i--)
+                {
+
+                    ReverseNumberstr += Numberstr[i];
+                }
+            }
+            
+            int ReverseNumber=Convert.ToInt32(ReverseNumberstr);
+            Console.WriteLine(Sign+""+ReverseNumber);
+            return ReverseNumber;
+        }
+        static void Main(string[] args)
+        {
+            Program p =new Program();
+            Console.WriteLine("Enter a Number:");
+            int Number=Convert.ToInt32(Console.ReadLine());
+            p.ReverseIntegerNumber(Number);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCode/ReverseNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        public int ReverseNumbers(int Number)
+        {
+            int ReveresNumber=0;
+            int Remaining;
+            while(Number != 0)
+            {
+                Remaining = Number % 10;
+                ReveresNumber= ReveresNumber * 10+ Remaining;
+                Number /= 10;
+            }
+            Console.WriteLine("Reverse number is " +ReveresNumber);
+            return ReveresNumber;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(p.ReverseNumbers(Number));
+            Console.ReadLine();
+
+        }
+    }
+}

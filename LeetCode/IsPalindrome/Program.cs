89bdc22d5f340a5ded928a4d70343e09dd45a9b8
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-23 14:29:23 +0530

    LeetCode Problem of regular pattern Matching

---
+++ LeetCode/IsPalindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPalindrome
+{
+    internal class Program
+    {
+        public bool IsPalindrome(int x)
+        {
+            int ReveresNumber = 0;
+            int Remaining;
+            bool result = true;
+            while (x != 0)
+            {
+                Remaining = x % 10;
+                ReveresNumber = ReveresNumber * 10 + Remaining;
+                x /= 10;
+            }
+            if (ReveresNumber.Equals(x))
+                
+            {
+                Console.WriteLine(result);
+
+            }
+            else
+            {
+                result = false;
+                Console.WriteLine(result);
+
+            }
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int x = Convert.ToInt32(Console.ReadLine());
+            p.IsPalindrome(x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCode/RegularExpressionMatching/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionMatching
+{
+    internal class Program
+    {
+        public bool IsMatch(string InputString,string Pattern)
+        {
+            bool Match = true;
+            
+            for (int j = 0; j < Pattern.Length; j++)
+            {
+                if (Pattern.Contains(".*"))
+                {
+                    for (int i = 0; i < InputString.Length; i++)
+                    {
+                        if (InputString != "")
+                        {
+                            return Match;
+                        }
+                        else
+                        {
+                            Match = false;
+                            return Match;
+                        }
+
+                    }
+
+                }
+                else if (Pattern.Contains("."))
+                {
+                    for(int i = 0; i < InputString.Length; i++)
+                    {
+                        if (InputString.Contains(Pattern[i]))
+                        {
+                            return Match;
+                        }
+                        else
+                        {
+                            Match = false;
+                            return Match;
+                        }
+
+                    }
+                }
+                else if (Pattern.Contains("*"))
+                {
+                    for (int i = 0; i < InputString.Length; i++)
+                    {
+                        if (InputString.Contains(Pattern[i]))
+                        {
+                            return Match;
+                        }
+                        else
+                        {
+                            Match = false;
+                            return Match;
+                        }
+
+                    }
+
+                }
+                
+                else if (Pattern.Equals(InputString))
+                {
+                    return Match;
+                }
+                else
+                {
+                    Match = false;
+                    return Match;
+                }
+            }
+            
+            return Match;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Input String here");
+            string InputString=Console.ReadLine();
+            Console.WriteLine("Enter a Pattern here");
+            string Pattern = Console.ReadLine();
+            int patternLength=Pattern.Length;
+            
+            Program p = new Program();
+            Console.WriteLine(p.IsMatch(InputString, Pattern));
+            Console.ReadLine();
+
+
+        }
+    }
+}

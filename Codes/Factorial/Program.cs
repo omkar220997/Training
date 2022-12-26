5a31996aabb9cab99087e70f5ed3807857610eae
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-26 12:25:54 +0530

    Factorial of Number

---
+++ Codes/Factorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Factorial = 1;
+            Console.WriteLine("Enter Your Number Here:");
+            int Number=Convert.ToInt32(Console.ReadLine());
+            if (Number == 0)
+            {
+                Console.WriteLine("Factorial of 0 is 1");
+            }
+            else if(Number == 1)
+            {
+                Console.WriteLine("Factorial of 1 is 1");
+            }
+            else if (Number > 1)
+            {
+                for(int i = 1; i <= Number; i++)
+                {
+                    Factorial*=i;
+                }
+                Console.WriteLine($"Factorial of {Number} is {Factorial}");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}

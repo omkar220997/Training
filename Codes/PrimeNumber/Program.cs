46548abccf2543784eac74c9153951e1344f9e57
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-26 09:47:03 +0530

    Prime Number check

---
+++ Codes/PrimeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            Console.WriteLine("Enter Number Here To Check");
+            Number=Convert.ToInt32(Console.ReadLine());
+            if (Number == 0 || Number == 1)
+            {
+                Console.WriteLine("O or 1 is not a Prime Number Enter value greater than 1");
+            }
+            else if (Number % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is Not Prime");
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is Prime");
+            }
+            Console.ReadLine();
+        }
+    }
+}

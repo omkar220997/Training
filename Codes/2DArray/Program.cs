eb9e40fde16871ed82092e3cc26dbb0ed9e46fca
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-27 17:27:38 +0530

    Addition of two 2D Arrays

--- Codes/2DArray/Program.cs
+++ Codes/2DArray/Program.cs
@@ -33,6 +33,52 @@ namespace _2DArray
 
                 }
             }
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter the size of second 2D-Array");
+            int FirstSizeOfSecondArray =Convert.ToInt32(Console.ReadLine());
+            int SecondSizeOfSecondArray =Convert.ToInt32(Console.ReadLine());
+            int[,] SecondOriginal2DArray = new int[FirstSizeOfSecondArray, SecondSizeOfSecondArray];
+            Console.WriteLine("Enter the elements of 2D-Array");
+            for( int i = 0; i < FirstSizeOfSecondArray; i++)
+            {
+                for( int j = 0; j < SecondSizeOfSecondArray; j++)
+                {
+                    Console.WriteLine($"Enter the element for {i} , {j} position");
+                    SecondOriginal2DArray[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+            for (int i = 0; i < FirstSizeOfSecondArray; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < SecondSizeOfSecondArray; j++)
+                {
+                    Console.Write("{0}\t", SecondOriginal2DArray[i, j]);
+
+                }
+            }
+            Console.WriteLine("\n");
+
+            int[,] Addition2DArray=new int[FirstSizeOfSecondArray, SecondSizeOfSecondArray];
+            for (int i = 0; i < FirstSizeOfSecondArray; i++)
+            {
+                for (int j = 0; j < SecondSizeOfSecondArray; j++)
+                {
+                    
+                    Addition2DArray[i, j] = Original2DArray[i, j] + SecondOriginal2DArray[i,j];
+                }
+
+            }
+            for (int i = 0; i < FirstSizeOfSecondArray; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < SecondSizeOfSecondArray; j++)
+                {
+                    Console.Write("{0}\t", Addition2DArray[i, j]);
+
+                }
+            }
+
             Console.ReadLine();
 
         }

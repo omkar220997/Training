dda98c8593c70b5192babd94a9c9c27f432a9b8f
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-27 09:18:08 +0530

    Solid Principles

---
+++ SOLID Principles/PolicyRating/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyRating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insurance policy rating system is starting....");
+            var engine = new RatingEngine();
+            engine.Rate();
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating: {engine.Rating}");
+            }
+            else
+            {
+                Console.WriteLine("No rating Produced");
+            }
+        }
+    }
+}
---
+++ SOLID Principles/PolicyRating/RatingEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyRating
+{
+    internal class RatingEngine
+    {
+        public decimal Rating { get; set; }
+        public void Rate()
+        {
+
+        }
+    }
+}

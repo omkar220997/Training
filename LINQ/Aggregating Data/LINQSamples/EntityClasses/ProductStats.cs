2c27d536b55ef6be13f5f2122b8a4e5b287e99e6
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-28 17:29:57 +0530

    Count aggression used to count the products

---
+++ LINQ/Aggregating Data/LINQSamples/EntityClasses/ProductStats.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LINQSamples
+{
+  public partial class ProductStats
+  {
+    public ProductStats()
+    {
+      Max = Decimal.MinValue;
+      Min = Decimal.MaxValue;
+
+      TotalProducts = 0;
+      Total = 0;
+    }
+
+    public int TotalProducts { get; set; }
+    public decimal Max { get; set; }
+    public decimal Min { get; set; }
+    public decimal Total { get; set; }
+    public decimal Average { get; set; }
+
+    public ProductStats Accumulate(Product prod)
+    {
+      // Increment total number of products
+      TotalProducts += 1;
+
+      // Add to total list price
+      Total += prod.ListPrice;
+
+      // Calculate Max and Min
+      Max = Math.Max(Max, prod.ListPrice);
+      Min = Math.Min(Min, prod.ListPrice);
+
+      return this;
+    }
+
+    public ProductStats ComputeAverage()
+    {
+      Average = Total / TotalProducts;
+
+      return this;
+    }
+  }
+}
---
+++ LINQ/Aggregating Data/LINQSamples/Readme.txt
@@ -0,0 +1,14 @@
+﻿To run this project in Visual Studio 2019, open the LINQSample.sln
+To run this project in Visual Studio Code, open the folder \LINQSamples
+
+SampleViewModel Samples
+--------------------------------------------------
+Count() - Count all rows in collection
+CountFiltered() - Count all rows after where clause is applied
+Sum() - Get a sum of a numeric column in a collection
+Minimum() - Get the minimum value from a numeric column in a collection
+Maximum() - Get the maximum value from a numeric column in a collection
+Average() - Get the average value from a numeric column in a collection
+AggregateSum() - Use the Aggregate() function to simulate the Sum() operation
+AggregateUsingGrouping() - Group products by Size property and calculate min/max/average prices
+AggregateUsingGroupingMoreEfficient() - Use an accumulator class to be more efficient

e528bcf25aa3271b0537ee3b885c678e289f9dc5
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-30 16:27:28 +0530

    null condition operator with arrays

--- Null/GameConsoleApp/Program.cs
+++ Null/GameConsoleApp/Program.cs
@@ -11,15 +11,28 @@ namespace GameConsoleApp
         static void Main(string[] args)
         {
             //var player = new PlayerCharacter();
-            PlayerCharacter player = new PlayerCharacter();
+            //PlayerCharacter player = new PlayerCharacter();
             //player.PlayerName="Omkar Kadam";
-           player.DaysSinceLastLogin = 22;
-            int days = player?.DaysSinceLastLogin ?? -1;
-            
+            //player.DaysSinceLastLogin = 22;
+            //int days = player?.DaysSinceLastLogin ?? -1;
+
             //player.DateOfBirth = new DateTime(1997, 11, 14);
             //player.IsPlayerNew= true;
-            Console.WriteLine(days);
+            //Console.WriteLine(days);
             //PlayerDisplayer.Write(player);
+
+            //PlayerCharacter[] players = new PlayerCharacter[3]
+            //{
+            //    new PlayerCharacter {PlayerName = "Omkar" },
+            //    new PlayerCharacter(),
+            //    null
+
+            //};
+            PlayerCharacter[] players = null;
+            string player1 = players?[0]?.PlayerName;
+            string player2 = players?[1]?.PlayerName;
+            string player3 = players?[2]?.PlayerName;
+
             Console.ReadLine();
         }
     }

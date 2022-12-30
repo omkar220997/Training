03c1ea908eb01301c4dd4e9fece366099f4df09c
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-30 10:00:14 +0530

    xUnit Test of Exception

---
+++ Exception Handling/02xUnit/ConsoleCalculator/ConsoleCalculator.Tests.xUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ConsoleCalculator.Tests.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ThrowWhenUnsupportedOperation()
+        {
+            var sut = new Calculator();
+            Assert.Throws<CalculationOperationNotSupportedException>(() => sut.Calculate(1, 1, "+"));
+            //Assert.Throws<CalculationException>(() => sut.Calculate(1, 1, "+"));
+            Assert.ThrowsAny<CalculationException>(() => sut.Calculate(1, 1, "+"));
+            var ex = Assert.Throws<CalculationOperationNotSupportedException>(() => sut.Calculate(1, 1, "+"));
+            Assert.Equal("+", ex.Operation);
+        }
+    }
+}

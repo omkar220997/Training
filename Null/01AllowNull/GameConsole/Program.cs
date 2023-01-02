39ee3ea0f9f184cdf51ebd1bc3aed15ba1e03d4f
omkar kadam <omkar.kadam@klingelnberg.com>
2023-01-02 14:39:26 +0530

    NotNull Postcondition attribute

--- Null/01AllowNull/GameConsole/Program.cs
+++ Null/01AllowNull/GameConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GameConsole
 {
@@ -8,10 +9,23 @@ namespace GameConsole
         {
             PlayerCharacter p1 = new();
             p1.Name = null;
-            p1.Bio = null;
-            Console.WriteLine(p1.Name);
-            Console.WriteLine(p1.Bio);
+            //p1.Bio = null;
+            Console.WriteLine("Please enter a bio");
+            string? bio = Console.ReadLine();
+            //Console.WriteLine(p1.Name);
+            ExitProgramIfNull(bio, "bio information");
+            p1.Bio = bio;
+            //Console.WriteLine(p1.Bio);
             Console.ReadLine();
+        }
+        static void ExitProgramIfNull([NotNull]string? input, string inputDescription)
+        {
+            if (input is null)
+            {
+                Console.WriteLine($"{inputDescription} was null, exiting program.");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
         }
     }
 }

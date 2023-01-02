1d45a868e1013b70ac1a55ae6277b69e84d5c12a
omkar kadam <omkar.kadam@klingelnberg.com>
2023-01-02 10:30:37 +0530

    Converting nullable references to non nullable references by using nullable directives

---
+++ Null/01NewConsoleApp/CSNullBasic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSNullBasic
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+#nullable enable
+            string message= null;
+#nullable disable
+            Console.WriteLine(message);
+
+            Console.WriteLine("Press Enter to end...");
+            Console.ReadLine();
+        }
+    }
+
+}
